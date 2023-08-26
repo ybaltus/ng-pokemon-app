dcd60b586789ef71d0b54b07e40dc3585e7bd18c
Yannis Baltus <yannis.baltus@outlook.com>
2023-08-26 15:19:37 +0200

    [PageNotFoun]Center elements

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page-not-found',
   template: `
-    <div fxLayout="column wrap" fxLayoutAlign="center">
-      <div fxFlex="50%">
+    <div fxLayout fxLayoutAlign="center center">
+      <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/035.png"/>
+      <div fxFlex="40%">
         <h1>Hey, cette page n'existe pas !</h1>
-        <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/035.png"/>
-        <button mat-raised-button color="accent" routerLink="/pokemons">
-          Retourner à l' accueil
+        <button mat-raised-button color="accent" routerLink="/pokemons" title="Retourner à l'accueil">
+          Retourner à l'accueil
         </button>
       </div>
     </div>
